375195d25f776436f23446cfb52148a864572384
ZionDere2 <165028736+ZionDere2@users.noreply.github.com>
2025-10-15 15:07:11 +0200

    Implement solutions for introductory exercises

--- 01_Aufgaben/01ex.c
+++ 01_Aufgaben/01ex.c
@@ -36,7 +36,7 @@ int simple_assignment(int a, int b) {
     die Codezeile `result = a + b;` zwischen diesen beiden Kommentaren einfügen, speichern, und dann weiterlesen.
     */
     /* BEGIN CHANGES */
-
+    result = a * b;
     /* END CHANGES */
     /*
     Die Codezeile, die Sie eingefügt haben, weist der Variable `result` die Summe der beiden Variablen `a` und `b` zu.
@@ -79,7 +79,8 @@ int nested_expressions(int x, int y) {
     selbstdefinierten Variable ab. Probieren Sie gerne beides.
     */
     /* BEGIN CHANGES */
-
+    int diff = x - y;
+    result = diff * diff;
     /* END CHANGES */
     return result;
 }
@@ -93,7 +94,10 @@ int rotation(int a, int b, int c) {
     die Belegung `a = 3, b = 7, c = 0` sein.
     */
     /* BEGIN CHANGES */
-
+    int original_a = a;
+    a = b;
+    b = c;
+    c = original_a;
     /* END CHANGES */
     return a + (b * 256) + (c * 256 * 256); /* Ja, diese Zeile mag überraschend sein. Einfach ignorieren :-) */
 }
@@ -107,7 +111,11 @@ int while_loop(int n) {
     `n` ist nie kleiner als `1`.
     */
     /* BEGIN CHANGES */
-
+    int i = 1;
+    while (i <= n) {
+        result *= i;
+        i++;
+    }
     /* END CHANGES */
     return result;
 }
@@ -121,7 +129,9 @@ int for_loop(int n) {
     `n` ist nie kleiner als `1`.
     */
     /* BEGIN CHANGES */
-
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
     /* END CHANGES */
     return result;
 }
@@ -136,7 +146,7 @@ int ones_place(int n) {
     Tipp: der `%` Operator sollte vermutlich in der Lösung auftauchen.
     */
     /* BEGIN CHANGES */
-
+    result = n % 10;
     /* END CHANGES */
     return result;
 }
@@ -151,7 +161,7 @@ int tens_place(int n) {
     Tipp: der `/` Operator sollte vermutlich in der Lösung auftauchen.
     */
     /* BEGIN CHANGES */
-
+    result = (n / 10) % 10;
     /* END CHANGES */
     return result;
 }
@@ -174,7 +184,7 @@ int fine_print1() {
     der Variable `result` die Anzahl der Sätze in diesem Kommentar zu.
     */
     /* BEGIN CHANGES */
-
+    result = 8;
     /* END CHANGES */
     return result;
 }
@@ -192,7 +202,7 @@ int fine_print2() {
     der Buchstaben des Nomens in dem Satzteil *zwischen den Sternchen* im vorigen Absatz zu.
     */
     /* BEGIN CHANGES */
-
+    result = 7;
     /* END CHANGES */
     return result;
 }
@@ -216,7 +226,7 @@ int fine_print3() {
     Um uns zu bestätigen, dass Sie das verstanden haben, weisen Sie der Variable `result` den Wert 6174 zu.
     */
     /* BEGIN CHANGES */
-
+    result = 6174;
     /* END CHANGES */
     return result;
 }
